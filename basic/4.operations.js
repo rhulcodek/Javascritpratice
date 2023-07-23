d17c9794fc59a1205ae8bd5c6277c63859f4d1e0
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2023-07-23 11:37:54 +0000

    code

---
+++ basic/4.operations.js
@@ -0,0 +1,20 @@
+//let value=2
+//let negvalue=-value
+//console.log(negvalue);
+// let str1="Hello"
+// let str2=" Rahul"
+// let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+
+
+let num1,num2,num3
+num1=num2=num3=2*2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
---
+++ basic/6.datatype.js
@@ -0,0 +1,30 @@
+//primitive 
+//7 types : string, number,boolean,null,undefined,symbol,bigint
+
+const score= 100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId =Symbol('123');
+console.log(id==anotherId);
+const bigNumber=3636982658642n
+
+
+
+
+
+//Reference (Non primitive )
+//Array,Objects,Functions
+
+const heros=["shaktiman","Naagraj","doga"];
+let myobj={
+    name:"Rahul",
+    age : 22,
+}
+const myFunction =function(){
+console.log("Hello Rahul");
+}
+console.log(typeof bigNumber);
