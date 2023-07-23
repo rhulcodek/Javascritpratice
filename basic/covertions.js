1d6d4c477b04b1a2ec5aed562dea799e01d8a4af
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2023-07-23 06:20:11 +0000

    convertion topic

---
+++ basic/covertions.js
@@ -0,0 +1,15 @@
+let score="Rahul"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIdentifier=Number(score)
+console.log(typeof valueIdentifier);
+console.log(valueIdentifier);
+
+//"33"=>33
+//"33abd"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn=1
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
