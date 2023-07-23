7703028e838bf0a7a4c0d614b602fc4f6e04d935
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2023-07-23 05:45:07 +0000

    code

--- basic/test.js
+++ basic/test.js
@@ -1,1 +1,15 @@
-console.log("Rahul choudhari");
+const accountId=123
+let accountEmail="rahul@gmail.com"
+var accountPassword="123"
+accountCity="Nagpur"
+let accountState; //not value declare but varibale undefined
+
+//accountId=2//not allowed
+
+accountEmail="vihu@gmail.com"
+accountPassword="123"
+accountCity="Wardha"
+
+console.log(accountId);
+/*prefer not to use var because of issue in block scope and funtion scope*/
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState]);
