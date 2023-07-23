9f83a77541260aefd087ff8e4615508df42a4f02
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2023-07-23 05:59:04 +0000

    datatype

---
+++ basic/datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+//alert (3+3)//we are using node js not broweser
+
+let name="Rahul"
+let age=26
+let isLoggedIn=false
+let state
+//number =>2 to power 53
+//bigint
+//String =>""
+//boolean
+//null => standAlone value
+//undefined=>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "Rahul");
+console.log(typeof undefined);
+console.log(typeof null);
