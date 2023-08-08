341ddf7e7a4b6dc372a5ed8ad0b8693d0f9587d9
Rahul Choudhari <59331583+rhulcodek@users.noreply.github.com>
2023-08-08 12:37:53 +0000

    objects

--- basic/12.object.js
+++ basic/12.object.js
@@ -40,7 +40,7 @@
 //const tinderuser=new Object()
 const tinderuser={}
 
-tinderuser.id="1234"
+tinderuser.id=123
 tinderuser.name="Rahul"
 tinderuser.isLogged=false
 //console.log(tinderuser);    
@@ -55,4 +55,36 @@ const regularUser={
     }
 }
 
-console.log(regularUser);
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj4={5:"a",6:"b"}
+//const obj3={obj1,obj2}
+
+//const obj3=Object.assign({},obj1,obj2,obj4)
+const obj3={...obj1,...obj4}
+//console.log(obj3);
+
+
+const users=[
+    {
+    id:1,
+    email:"rahul@gmail.com"
+},  {
+    id:2,
+    email:"rahul@gmail.com"
+},  {
+    id:3,
+    email:"rahul@gmail.com"
+}
+]
+
+users[1].email
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('isLogged'));
